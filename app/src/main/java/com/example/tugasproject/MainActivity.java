070204d8b69f532dd3fbc57d963fefa31861ebf1
abiyaca <almaarijmuhammad@gmail.com>
2020-03-31 00:39:41 +0700

    membuat sistem login sederhana menggunakan username dan password

---
+++ app/src/main/java/com/example/tugasproject/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tugasproject;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnExit;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnExit = (Button) findViewById(R.id.btnExit);
+
+        btnExit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //perintah untuk mengakhiri aplikasi
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                MainActivity.this.startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
